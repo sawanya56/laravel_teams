82a253482b1fd2d020e9f103d3d1a5ef71a51b6e
sawanya56 <tongnun74@gmail.com>
2023-11-24 15:45:55 +0700

    update

--- app/Models/AddStudent.php
+++ app/Models/AddStudent.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class AddStudent extends Model
 {
@@ -18,12 +19,14 @@ class AddStudent extends Model
 
     public function updateStudentStatusAdd($student_mail, $class_id, $student_status)
     {
+        DB::beginTransaction();
         AddStudent::where([
             ['class_id', '=', $class_id],
             ['student_mail', '=', $student_mail]
         ])->update([
             'add_success' => $student_status
         ]);
+        DB::commit();
     }
 
     public function studentAddExist($class_id, $student_mail)
